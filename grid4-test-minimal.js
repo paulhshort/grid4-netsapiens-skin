4a02e9eadf1b97ee2681df6ee958bc52766cb451
paulhshort <paulhshort@gmail.com>
2025-06-25 22:37:43 -0400

    Add minimal test JS to diagnose white screen issue

---
+++ grid4-test-minimal.js
@@ -0,0 +1,13 @@
+/**
+ * Grid4 Test Minimal
+ * Version: 1.0.0 - Absolute minimal test
+ */
+
+console.log('Grid4 Test Minimal loaded');
+
+// Just log that we're here
+if (typeof jQuery !== 'undefined') {
+    console.log('jQuery is available');
+} else {
+    console.log('jQuery is NOT available');
+}
